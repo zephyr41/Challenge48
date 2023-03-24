a64c9852cafc8c50256dd34a8485acc7625b0468
Nils <nils.jaudon@ynov.com>
2023-03-24 01:51:03 +0100

    chonteux

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
+<link rel="stylesheet" href="styles.css" class="css">
 <body>
+<section>
+        <h2 id="TitleonePageone">Free Gaming</h2>
+        <h3 id="TitletwoPagetwo">L'offre qui casse le game</h3>
+ </section>
     
 </body>
 </html>
